2fbb5b75c7c641f4d7181737c8c9a165487e3910
Francisco Jesús Latorre García <piti_fran@hotmail.com>
2017-11-21 00:09:22 +0100

    Clase Punto con sus dos atributos.

---
+++ src/arrays/Punto.java
@@ -0,0 +1,18 @@
+package arrays;
+
+/**
+ * @author Francisco Jesus Latorre Garcia <franlatorregarcia@gmail.com>
+ * 6. Crea un paquete llamado arrays. Crea en él la clase Punto con sus dos
+ * atributos (x e y) y la visibilidad adecuada. 7. Crea un constructor con
+ * parámetros que actualice el valor de los atributos correctamente. 8. Crea los
+ * métodos get para los atributos. 9. Crea un método llamado toString que
+ * devolverá un String que será la representación de dicho punto: (x, y). 10.
+ * Crea un método llamado distancia que devuelva la distancia entre el punto y
+ * otro pasado por parámetro. Para ello puedes utilizar la siguiente fórmula:
+ */
+public class Punto {
+
+    private double x;
+    private double y;
+
+}
