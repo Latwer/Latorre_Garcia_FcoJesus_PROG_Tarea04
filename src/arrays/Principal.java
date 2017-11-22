84ed3ebdf4e81f8b1b20ee587df7e6e4fef50697
Francisco Jesús Latorre García <piti_fran@hotmail.com>
2017-11-22 19:04:06 +0100

    Clase Principal.

---
+++ src/arrays/Principal.java
@@ -0,0 +1,22 @@
+package arrays;
+
+/**
+ * @author Francisco Jesus Latorre Garcia <franlatorregarcia@gmail.com>
+ * Crea una clase, dentro del paquete arrays, llamada Principal que incluya un
+ * método main. Haz un commit. Crea el código necesario para pedir cuántos
+ * triángulos queremos generar y cree un array de Puntos de dicho tamaño. Haz un
+ * commit. Añade el código necesario para que se generen aleatoriamente los
+ * vértices de cada triángulo, para todos los triángulos. Haz un commit. Añade
+ * el código necesario para calcular el perímetro medio de todos los triángulos
+ * y lo muestre al usuario. Haz un commit. Añade el código necesario para
+ * calcular cuántos triángulos tienen un perímetro mayor o igual a la media y
+ * cuántos menor e informe al usuario. También debe de quedarse con el triángulo
+ * de mayor perímetro y de menor perímetro y mostrar la información de cada uno
+ * de ellos. Haz un commit y un push.
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+
+    }
+}
