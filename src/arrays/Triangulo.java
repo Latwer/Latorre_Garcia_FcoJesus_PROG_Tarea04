cf5e918cb7f105b327ceee93af707d4c1d1f3bd4
Francisco Jesús Latorre García <piti_fran@hotmail.com>
2017-11-22 14:31:42 +0100

    Clase Triangulo

---
+++ src/arrays/Triangulo.java
@@ -0,0 +1,24 @@
+package arrays;
+
+/**
+ * @author Francisco Jesus Latorre Garcia <franlatorregarcia@gmail.com>
+ * Crea, dentro del paquete arrays, una clase llamada Triangulo con sus tres
+ * vértices como atributos y con la visibilidad adecuada. Haz un commit. Crea un
+ * constructor para esta clase que acepte como parámetros los vértices del
+ * triángulo y los asigne a los atributos. Haz un commit. Crea los métodos get
+ * para los diferentes atributos con su visibilidad adecuada. Haz un commit.
+ * Crea un método llamado toString que devolverá un String que será la
+ * representación de dicho triángulo: A(x, y) B(x, y) C(x, y). Haz un commit.
+ * Crea un método llamado perímetro que devolverá el perímetro del triángulo. El
+ * perímetro de un triángulo se halla sumando sus lados. Por lo que deberás
+ * hallar los tres lados (AB, AC, BC) y sumarlo. Para hllar cada lado puedes
+ * recurrir a la distancia entre un punto y otro implementada en la clase Punto.
+ * Haz un commit.
+ */
+public class Triangulo {
+
+    private double vertice1;
+    private double vertice2;
+    private double vertice3;
+
+}
